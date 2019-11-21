718324c23b1fbc44aa35dac04d4f0ac25380423e
ShresthaGaurav <gauravshrestha202@gmail.com>
2019-11-21 22:54:10 +0545

    initial design  of Sign up

--- app/src/main/java/com/ShresthaGaurav/food_recipe/MainActivity.java
+++ app/src/main/java/com/ShresthaGaurav/food_recipe/MainActivity.java
@@ -2,13 +2,70 @@ package com.ShresthaGaurav.food_recipe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+EditText log_name,log_pass;
+Button btn_log,btn_sign;
+ImageButton btn_showpass;
+TextView txthelp;
+    private boolean isShowPassword = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        log_name=findViewById(R.id.edLname);
+        log_pass=findViewById(R.id.edLpass);
+        btn_log=findViewById(R.id.btnlog);
+        btn_sign=findViewById(R.id.btnsign);
+        txthelp=findViewById(R.id.textHelp);
+        btn_showpass=findViewById(R.id.btn_showpass);
+        log_pass.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                btn_showpass.setVisibility(View.VISIBLE);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        btn_showpass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isShowPassword) {
+                    log_pass.setTransformationMethod(new PasswordTransformationMethod());
+                    btn_showpass.setImageDrawable(getResources().getDrawable(R.drawable.ic_panorama_fish_eye_black_24dp));
+                    isShowPassword = false;
+                }else{
+                    log_pass.setTransformationMethod(null);
+                    btn_showpass.setImageDrawable(getResources().getDrawable(R.drawable.ic_remove_red_eye_black_24dp));
+                    isShowPassword = true;
+                }
+            }
+        });
+        btn_sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sinunwin= new Intent(MainActivity.this,SignUp.class);
+                startActivity(sinunwin);
+            }
+        });
     }
 }
--- app/src/main/java/com/ShresthaGaurav/food_recipe/SignUp.java
+++ app/src/main/java/com/ShresthaGaurav/food_recipe/SignUp.java
@@ -2,13 +2,130 @@ package com.ShresthaGaurav.food_recipe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class SignUp extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class SignUp extends AppCompatActivity implements View.OnClickListener {
+    EditText sn_first, sn_last, sn_pass, sn_repass;
+    ImageButton btn_shoP, btn_showRP;
+    TextView sn_dob;
+    private boolean isShowPassword = false;
+    private boolean isShowREPassword = false;
+    Calendar myCalendar = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        sn_first = findViewById(R.id.sn_fname);
+        sn_last = findViewById(R.id.sn_lname);
+        sn_dob = findViewById(R.id.sn_dob);
+        sn_pass = findViewById(R.id.sn_pass);
+        sn_repass = findViewById(R.id.sn_repass);
+        btn_shoP = findViewById(R.id.btn_showpassP);
+        btn_shoP.setOnClickListener(this);
+        btn_showRP = findViewById(R.id.btn_showpassRP);
+        btn_showRP.setOnClickListener(this);
+        final DatePickerDialog.OnDateSetListener setdate = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, month);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                String myFormat = "MMMM /EEEE /yyyy "; //In which you need put here
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+                sn_dob.setText(sdf.format(myCalendar.getTime()));
+            }
+        };
+        sn_dob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(SignUp.this, setdate, myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+
+            }
+        });
+        sn_pass.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                btn_shoP.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        sn_repass.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                btn_showRP.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_showpassP:
+
+                if (isShowPassword) {
+                    sn_pass.setTransformationMethod(new PasswordTransformationMethod());
+                    btn_shoP.setImageDrawable(getResources().getDrawable(R.drawable.ic_panorama_fish_eye_black_24dp));
+                    isShowPassword = false;
+                    return;
+                } else {
+                    sn_pass.setTransformationMethod(null);
+                    btn_shoP.setImageDrawable(getResources().getDrawable(R.drawable.ic_remove_red_eye_black_24dp));
+                    isShowPassword = true;
+                    return;
+                }
+            case R.id.btn_showpassRP:
+                if (isShowREPassword) {
+                    sn_repass.setTransformationMethod(new PasswordTransformationMethod());
+                    btn_showRP.setImageDrawable(getResources().getDrawable(R.drawable.ic_panorama_fish_eye_black_24dp));
+                    isShowREPassword = false;
+                    return;
+                } else {
+                    sn_repass.setTransformationMethod(null);
+                    btn_showRP.setImageDrawable(getResources().getDrawable(R.drawable.ic_remove_red_eye_black_24dp));
+                    isShowREPassword = true;
+                    return;
+                }
+
+        }
+
     }
 }
